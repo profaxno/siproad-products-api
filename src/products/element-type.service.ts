7890c48f253d4d378ace1e4c8c2ad594d134d7e4
Ivan Miguel Perez Torres <ivanmperezt@gmail.com>
2025-03-17 10:37:37 -0300

    Implement: SIPR-11 - Upgrade (apis) LIKE query - Delete (bff) companies resolver - Delete (sales) multiplication cost  * qty

--- src/products/element-type.service.ts
+++ src/products/element-type.service.ts
@@ -269,7 +269,7 @@ export class ElementTypeService {
           company: {
             id: companyId
           },
-          name: Raw( (fieldName) => inputDto.searchList.map(value => `${fieldName} LIKE '%${value}%'`).join(' OR ') ),
+          name: Raw( (fieldName) => inputDto.searchList.map(value => `${fieldName} LIKE '%${value.replace(' ', '%')}%'`).join(' OR ') ),
           // name: In(inputDto.searchList),
           active: true
         }
--- src/products/element.service.ts
+++ src/products/element.service.ts
@@ -297,7 +297,7 @@ export class ElementService {
           company: {
             id: companyId
           },
-          name: Raw( (fieldName) => inputDto.searchList.map(value => `${fieldName} LIKE '%${value}%'`).join(' OR ') ),
+          name: Raw( (fieldName) => inputDto.searchList.map(value => `${fieldName} LIKE '%${value.replace(' ', '%')}%'`).join(' OR ') ),
           // name: In(inputDto.searchList),
           active: true
         }
